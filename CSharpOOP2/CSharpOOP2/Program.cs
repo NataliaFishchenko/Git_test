32682236ae223a29ec3f72e8346bf9b83d8eafdb
Natalia Fishchenko <natallia.fishchanka@gmail.com>
2022-11-11 11:41:28 -0700

    Homework 7 t.1-17

---
+++ CSharpOOP2/CSharpOOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using CSharpOOP2;
+
+var tableWhite = new Table();
+tableWhite.Color = "White";
+tableWhite.Height = 70;
+tableWhite.Material = "Wood";
+tableWhite.IsForKitchen = false;
+//tableWhite.Length = 150; //Error Line
+tableWhite.Width = 50;
+
+var tableBraun = new Table {Color = "Braun", Height = 50, Material = "Wood", IsForKitchen = true};
+
+var tableBlack = new Table("Black", 40, "Iron", false);
+
+var tableIron = new Table {Height = 50, Material ="Iron", IsForKitchen = false};
+
+Table.TaskForYou();
+
+tableBlack.RoomForTable();
+
+//tableIron.RoomForTable(); //Error Line because Color = null
+
+tableWhite.AddShelves = 3;
+//var numberShelves = tableWhite.AddShelves; //Error Line
+
+var priceForShelves = tableBraun.PriceForShelves;
+//tableBraun.PriceForShelves = 135; //Error line
+
+Console.WriteLine(tableWhite.State);
+
+var type = Table.Type; //вызов статического поля
+
+
+
+
+
+
+
